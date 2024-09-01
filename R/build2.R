7cb6b5f3c201434105c0e19fac697e3c52f8adb6
Ricardo Theodoro <rtheodoro@usp.br>
2024-09-01 19:06:25 -0300

    Update build2.R

--- R/build2.R
+++ R/build2.R
@@ -1,8 +1,5 @@
 # An optional custom script to run after Hugo builds your site.
 # You can delete it if you do not need it.
-Host:
-_atproto
-Type:
-TXT
-Value:
-did=did:plc:x6spkvvf6k7a4epm6336pyeg
+Host: _atproto
+Type: TXT
+Value: did=did:plc:x6spkvvf6k7a4epm6336pyeg
